36e81832de9427a0d73b58a33a49d2294699a881
Ayah Kailani <ayah.kailani@gmail.com>
2022-01-23 17:15:29 +0200

    Add files via upload

---
+++ copy to clipboard jQuery/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  //two functions to add and remove the class in span
+  function add() {
+    $(".copied").addClass("bounce-effect");
+  }
+  function remove() {
+    $(".copied").removeClass("bounce-effect");
+  }
+  // Call the functions and copy the text on button click
+  $(".copy-btn").click(function () {
+    $("#textField").select();
+    document.execCommand("copy");
+    add();
+    setTimeout(remove, 800);
+  });
+});
